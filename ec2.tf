61545f9e4287a48d696ea0e09aebec5e1ce8305a
puneethirvichetty <puneeth2011@gmail.com>
2020-03-18 14:05:30 +0000

    commit

--- ec2.tf
+++ ec2.tf
@@ -3,3 +3,34 @@ provider "google" {
   project     = "cloudglobaldelivery-1000135575"
   region      = "us-central1"
 }
+resource "google_compute_instance" "default" {
+  name         = "test34"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+}
